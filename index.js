11cc3a22e90945a5091d7131c8c3ab2e7837fc27
Ubuntu <ubuntu@ip-172-31-25-39.ec2.internal>
2020-12-01 23:35:35 +0000

    temp2

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+const db = require("./models");
+const morgan  = require('morgan');
+const express = require('express');
 
-const express = require('express')
 const app = express();
 //app.use(express.json());
 const port = 8000;
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
 app.set("view engine", "ejs")
 app.get('/',(req,res) =>{
+
 	res.render('index')
 });
 
@@ -12,7 +17,39 @@ app.post('/ingreso', function (req, res) {
   const params = req.body;
 	console.log(params);
 });
+app.get('/admin/add',async function (req,res){
+	console.log(req.body);
+	const User =db.user;
+	const users =await User.findAll();
+	const mappedUsers = users.map(function(user){
+		return {
+		name:user.name+" "+user.lastName,
+		dni:user.identification,
+		rol:user.role
 
+		};
+	});
+	res.render('admin',{object:mappedUsers});
+});
+
+app.post('/admin/add',async function(req,res){
+	const User=db.user;
+	const params=req.body;
+	const parsedParams={
+		identification:params.dni,
+		name:params.name,
+		lastName:params.lastname,
+		username:params.username,
+		password:params.pass
+	};
+	const newUser = await User.create(parsedParams);
+	console.log(newUser);
+	const users =await User.findAll();
+	console.log("////////////////////////////////////////////////////////////////////////////////");
+	console.log(req.body);
+	res.render('admin',{object:users})
+})
+	
 app.listen(8000, () => {
   console.log('Example app listening on port 8000!')
 });
